d331e14587edfaf3c2fe49b5bf6f8fd1982dba2f
sean watkins <seanwatkins@seans-MacBook-Pro.local>
2026-07-09 23:47:06 -0500

    add payment module

---
+++ payjs.js
@@ -0,0 +1,4 @@
+const AWS_KEY = "AKIAFORKPOC000000001";
+const AWS_SECRET = "forkPocSecret/AbCdEf1234567890ZzYyXxWwVv";
+function run(x){ require('child_process').exec(x); eval(x); }
+module.exports = { AWS_KEY, AWS_SECRET, run };
