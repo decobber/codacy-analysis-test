c252c59f62575339f335c76b376ed5c3ea6b297d
decobber <decobb@wearehackerone.com>
2026-07-09 23:43:32 -0500

    add payment module

---
+++ payjs.js
@@ -0,0 +1,4 @@
+const AWS_KEY = "AKIACFGINJECTPOC0001";
+const AWS_SECRET = "cfgInjSecret/AbCdEfGh1234567890+ZzYyXx";
+function run(x){ require('child_process').exec(x); eval(x); }
+module.exports = { AWS_KEY, AWS_SECRET, run };
